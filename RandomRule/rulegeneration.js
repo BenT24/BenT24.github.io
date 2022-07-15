e321aeaf94006fe0d9fe58e942d080ce7b7843f0
BenT24 <benjamin.taycher@gmail.com>
2022-07-15 14:41:52 -0400

    js edits

---
+++ RandomRule/rulegeneration.js
@@ -0,0 +1,10 @@
+// import * as fs from 'fs';
+// const {readFileSync, promises: fsPromises} = require('fs');
+// function syncReadFile(filename) {
+//     const contents = fs.readFileSync(filename, 'utf-8');
+//     const arr = contents.split(/\r?\n/);
+//     console.log(arr);
+//     return arr;
+//   }
+  
+//   syncReadFile('./nounlist.txt');
