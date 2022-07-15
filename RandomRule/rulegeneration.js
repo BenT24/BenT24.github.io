bb7df9236bb1c386c8576d71c1e18a202742fc2f
BenT24 <benjamin.taycher@gmail.com>
2022-07-15 16:41:16 -0400

    Update rulegeneration.js

--- RandomRule/rulegeneration.js
+++ RandomRule/rulegeneration.js
@@ -8,3 +8,16 @@
 //   }
   
 //   syncReadFile('./nounlist.txt');
+
+const nouns = ["post", "scandal", "increase", "descent", "grain"];
+const verbs = ["crush", "own", "chat", "experience", "sue"];
+const prepositions =["in", "on", "beside", "under", "above"]
+
+let noun1 = nouns[Math.floor(Math.random()*nouns.length)];
+let noun2 = nouns[Math.floor(Math.random()*nouns.length)];
+let verb1 = verbs[Math.floor(Math.random()*verbs.length)];
+let preposition1 = prepositions[Math.floor(Math.random()*prepositions.length)];
+
+let ret = "Always" + verb1 + "the" + noun1 + preposition1 + "a" + noun2;
+
+document.getElementById("rule").innerHTML = ret;
